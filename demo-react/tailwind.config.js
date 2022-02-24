fde3c25fe332c05ef03bd3c8c188b5be6840186d
Suwat Inkaew <swik999@gmail.com>
2022-02-24 16:28:07 +0700

    try to config bg image

--- demo-react/tailwind.config.js
+++ demo-react/tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: [ "./src/**/*.{js,jsx,ts,tsx}", ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        'bg': "url('demo-react/src/Image/bg.jpg')",
+       })
+    },
   },
   plugins: [],
 }
