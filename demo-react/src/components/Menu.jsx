6c6c4ad2d6470158978cab3be1ad4a15da303f55
Suwat Inkaew <swik999@gmail.com>
2022-02-24 16:27:33 +0700

    try to adjust menu

---
+++ demo-react/src/components/Menu.jsx
@@ -0,0 +1,13 @@
+const Menu = () => {
+    return (
+        <body>
+            <div 
+            class="w-full h-full bg-no-repeat bg-cover bg-center bg-fixed"
+            style="background-image: url('Image/bg.jpg');">
+            <a href="demo-react\src\components\Game.jsx"><img src ="demo-react\src\Image\PlayButton.png"></img></a>
+            </div>
+        </body>
+    )
+}
+
+export default Menu
