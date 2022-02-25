2d5f2c15540daa6c4418086e80e9c6d897af63db
Suwat Inkaew <swik999@gmail.com>
2022-02-25 18:35:50 +0700

    try to fix bug

--- demo-react/src/components/Menu.jsx
+++ demo-react/src/components/Menu.jsx
@@ -1,13 +1,11 @@
 ///still bug
 const Menu = () => {
     return (
-        <body>
-            <div 
-            class="w-full h-full bg-no-repeat bg-cover bg-center bg-fixed"
-            style="background-image: url('Image/bg.jpg');">
+        <div 
+            className="w-full h-full bg-no-repeat bg-cover bg-center bg-fixed"
+            styles="background-image: url('Image/bg.jpg');">
             <a href="demo-react\src\components\Game.jsx"><img src ="demo-react\src\Image\PlayButton.png"></img></a>
-            </div>
-        </body>
+        </div>
     )
 }
 
