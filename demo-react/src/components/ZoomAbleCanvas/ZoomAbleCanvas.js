ab7e4b9e2642758c8682239d2638c915996404d2
Suwat Inkaew <swik999@gmail.com>
2022-02-28 01:37:58 +0700

    Fix and add zoom button

---
+++ demo-react/src/components/ZoomAbleCanvas/ZoomAbleCanvas.js
@@ -0,0 +1,29 @@
+import { Canvas } from "components";
+import React, { Component } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import styles from './styles.module.css';
+
+export class ZoomAbleCanvas extends Component {
+    render() {
+      return (
+        <TransformWrapper
+          initialScale={1}
+          initialPositionX={0}
+          initialPositionY={0}
+        >
+          {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+            <React.Fragment>
+              <div className="tools w-72 bg-gray-200 justify-center text-center">
+                <button className={styles.button} onClick={() => zoomIn()}>ZoomIn</button>
+                <button className={styles.button} onClick={() => zoomOut()}>ZoomOut</button>
+                <button className={styles.button} onClick={() => resetTransform()}>Reset</button>
+              </div>
+              <TransformComponent>
+                <Canvas></Canvas>
+              </TransformComponent>
+            </React.Fragment>
+          )}
+        </TransformWrapper>
+      );
+    }
+  }
