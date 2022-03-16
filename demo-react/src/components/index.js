da2f2c54d33971c41dfa43f359dfa0e47bffab35
Suwat Inkaew <swik999@gmail.com>
2022-03-17 00:59:33 +0700

    delete non use element

--- demo-react/src/components/index.js
+++ demo-react/src/components/index.js
@@ -1,12 +1,10 @@
-export * from './Antibody';
+
 export * from './App';
 export * from './Canvas';
-export * from './Cell';
 export * from './Game';
 export * from './SelectAntibody';
 export * from './StartMenu';
 export * from './Tabbar';
-export * from './Virus';
 export * from './ZoomAbleCanvas';
 export * from './script';
 export * from './GlobalVar';
