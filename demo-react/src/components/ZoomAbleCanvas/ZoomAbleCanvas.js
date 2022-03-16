c224dc858c371eccc403d8f5a0f22cf6280f04dd
Suwat Inkaew <swik999@gmail.com>
2022-03-16 21:44:09 +0700

    disable doubleclick

--- demo-react/src/components/ZoomAbleCanvas/ZoomAbleCanvas.js
+++ demo-react/src/components/ZoomAbleCanvas/ZoomAbleCanvas.js
@@ -10,6 +10,7 @@ export class ZoomAbleCanvas extends Component {
           initialScale={1}
           initialPositionX={0}
           initialPositionY={0}
+          doubleClick={false}
           alignmentAnimation={{ sizeX: 0, sizeY: 0 }}
         >
           {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
